472de751e22f845fe2a17a13edfde976f3a2c232
ppawar <pravin.pawar@nbic.nl>
2013-06-27 17:22:35 +0200

    Started programming the logic for reading and writing project records.

--- src/nl/ctmm/trait/proteomics/ephrin/input/TxtDirectoryReader.java
+++ src/nl/ctmm/trait/proteomics/ephrin/input/TxtDirectoryReader.java
@@ -2,18 +2,36 @@ package nl.ctmm.trait.proteomics.ephrin.input;
 
 import java.io.File;
 
-public class TxtDirectoryReader {
-	
-	public TxtDirectoryReader (String txtDirectoryName) {
-	    final File txtDirectory = new File(txtDirectoryName);
-	    if (txtDirectory.exists()) {
-	        final File[] files = txtDirectory.listFiles();
-	        if (files != null) {
-	            for (final File file : files) {
-	               System.out.println("File found " + file.getAbsolutePath());
-	            }
-	        }
-	    }
-	}
+import nl.ctmm.trait.proteomics.ephrin.output.SummaryFileWriter;
+import nl.ctmm.trait.proteomics.ephrin.utils.Constants;
 
+public class TxtDirectoryReader {
+    
+    public TxtDirectoryReader (String txtDirectoryName) {
+        final File txtDirectory = new File(txtDirectoryName);
+        if (txtDirectory.exists()) {
+            File txtParent = txtDirectory.getParentFile(); 
+            String projectName = txtParent.getParentFile().getName();
+            String projectFolder = txtParent.getParentFile().getAbsolutePath();
+            String templateFilePath = "";
+            String secondLine = "";
+            final File[] files = txtDirectory.listFiles();
+            if (files != null) {
+                for (final File file : files) {
+                   if (file.getName().equals(Constants.PROPERTY_TEMPLATE_FILE_NAME)) {
+                       System.out.println("Template file found: " + file.getName() + 
+                               " Absolute path: " + file.getAbsolutePath());
+                       templateFilePath = file.getAbsolutePath();
+                       TemplateFileReader templateFileReader = new TemplateFileReader(templateFilePath);
+                       secondLine = templateFileReader.readSecondLine();
+                       break;
+                   }
+                }
+            }
+            System.out.println("Record: " + projectName + " " + secondLine + " " 
+                    + projectFolder);
+            ProjectRecordUnit projectrecordUnit = new ProjectRecordUnit(projectName, secondLine, projectFolder);
+            SummaryFileWriter.getInstance().addProjectRecordUnit(projectrecordUnit); 
+        }
+    }
 }
--- src/nl/ctmm/trait/proteomics/ephrin/output/SummaryFileWriter.java
+++ src/nl/ctmm/trait/proteomics/ephrin/output/SummaryFileWriter.java
@@ -1,5 +1,14 @@
 package nl.ctmm.trait.proteomics.ephrin.output;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+import nl.ctmm.trait.proteomics.ephrin.input.ProjectRecordUnit;
+import nl.ctmm.trait.proteomics.ephrin.utils.Constants;
+
 /**
  * Write resutls from the table to SummaryFile in Tab delimited format
  * @author opl
@@ -7,5 +16,25 @@ package nl.ctmm.trait.proteomics.ephrin.output;
  */
 
 public class SummaryFileWriter {
+	private static SummaryFileWriter instance = new SummaryFileWriter();
+    final File summaryFile = new File(Constants.PROPERTY_SUMMARY_FILE_FULLPATH);
+	
+    public static SummaryFileWriter getInstance() {
+    	if (instance == null) {
+    		instance = new SummaryFileWriter();
+    	}
+      return instance;
+    }
 
+	public void addProjectRecordUnit(ProjectRecordUnit projectRecordUnit) {
+        try {
+            FileWriter fWriter = new FileWriter(summaryFile, true);
+            BufferedWriter bWriter = new BufferedWriter(fWriter);
+            bWriter.write(projectRecordUnit.getProjectName() + "\t" + 
+            		projectRecordUnit.getFirstRawFile() + "\t" + projectRecordUnit.getFolderPath() + "\n");
+            bWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
 }
